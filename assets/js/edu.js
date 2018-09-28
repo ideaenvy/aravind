a71b321c735cbfea01f9a0f3f867929d3823b287
[Yatish] <[yatishpro@gmail.com]>
2018-09-28 18:24:44 +0530

    'footer changed'

--- assets/js/edu.js
+++ assets/js/edu.js
@@ -1,5 +1,23 @@
 window.onload = function(){
  
+  var eduTraining = $('.eduTrainingSlider');
+  eduTraining.owlCarousel({
+    items: 1, //10 items above 1000px browser width
+    itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+    itemsTablet: [600, 1], //2 items between 600 and 0
+    itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+});
+
+   eduTraining.owlCarousel({
+        items: 1, //10 items above 1000px browser width
+        itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsTablet: [600, 1], //2 items between 600 and 0
+        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+
   const subMenuEdu = document.querySelector('#eduSubMenu');
 
   subMenuEdu.addEventListener('click', function() {
