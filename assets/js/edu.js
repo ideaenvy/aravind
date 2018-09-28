72f8a70ec62f284020e18fb91cbb53f5096b83a6
[Yatish] <[yatishpro@gmail.com]>
2018-09-28 09:33:54 +0530

    "ent sub"

--- assets/js/edu.js
+++ assets/js/edu.js
@@ -11,4 +11,30 @@ window.onload = function(){
     }
   });
 
+  const longTermFellowship = document.querySelector('.btn-fellowship-t');
+  const shortTermFellowShip = document.querySelector('.btn-fellowship-s');
+
+  const longTermFellowshipContent = document.querySelector('.long-term-fellowship-content');
+  const shortTermFellowShipContent = document.querySelector('.short-term-fellowship-content');
+
+  longTermFellowship.addEventListener('click', function(){
+  
+
+    if(longTermFellowshipContent.style.display.toString() != 'none'){
+      longTermFellowshipContent.style.display = 'none';
+    } else{
+      longTermFellowshipContent.style.display = 'block';
+      shortTermFellowShipContent.style.display = 'none';
+    }
+  });
+
+  shortTermFellowShip.addEventListener('click', function(){
+
+    if(shortTermFellowShipContent.style.display.toString() != 'none'){
+      shortTermFellowShipContent.style.display = 'none';
+    } else {
+      shortTermFellowShipContent.style.display = 'block';
+      longTermFellowshipContent.style.display = 'none';
+    }
+  });
 }
