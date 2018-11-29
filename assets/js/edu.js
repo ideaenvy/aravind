1bd5976ac9f727e28fb69d170d9be125c46a5e04
[Yatish] <[yatishpro@gmail.com]>
2018-11-29 11:57:21 +0530

    'Css Minification'
    By Yatish

--- assets/js/edu.js
+++ assets/js/edu.js
@@ -9,14 +9,14 @@ window.onload = function(){
     itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
 });
 
-   eduTraining.owlCarousel({
-        items: 1, //10 items above 1000px browser width
-        itemsDesktop: [1000, 1], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
-        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
-    });
-
+   var owlCampus = $('.owl-campus');
+   owlCampus.owlCarousel({
+    items: 1, //10 items above 1000px browser width
+    itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+    itemsTablet: [600, 1], //2 items between 600 and 0
+    itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+});
 
   const subMenuEdu = document.querySelector('#eduSubMenu');
 
